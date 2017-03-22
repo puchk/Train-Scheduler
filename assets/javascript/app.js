cf34d4a3a37c5f563f8fad7159928603be53cefb
puchk <kpuchalski@gmail.com>
2017-03-22 00:27:40 -0700

    Updated to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,25 +10,68 @@ var config = {
 
 var database = firebase.database();
 
-$("#submitButton").on("click", function() {
+// Set Variables
+var name = "";
+var destination = "";
+var firstTrain = 0;
+var frequency = 0;
+
+$("#submitButton").on("click", function(event) {
 	event.preventDefault();
 
-	var name = $("#trainName").val().trim();
-	var destination = $("#destination").val().trim();
-	var firstTrain = $("#firstTrain").val().trim();
-	var frequency = $("#frequency").val().trim();
-
-	console.log(name, destination, firstTrain, frequency);
+	name = $("#trainName").val().trim();
+	destination = $("#destination").val().trim();
+	firstTrain = $("#firstTrain").val().trim();
+	frequency = $("#frequency").val().trim();
 
 	database.ref().push({
 		name: name,
 		destination: destination,
 		firstTrain: firstTrain,
-		frequency: frequency
+		frequency: frequency,
+		dateAdded: firebase.database.ServerValue.TIMESTAMP
+	});
+
+	$("#trainName").val("");
+	$("#destination").val("");
+	$("#firstTrain").val("");
+	$("#frequency").val("");
 	});
 
 	database.ref().on("child_added", function(childSnapshot) {
-		var snap = childSnapshot.val();
-		console.log(snap);
-	})
-});
+      name = childSnapshot.val().name;
+      destination = childSnapshot.val().destination;
+      firstTrain = childSnapshot.val().firstTrain;
+      frequency = childSnapshot.val().frequency;
+      dateAdded = childSnapshot.val().dateAdded;
+      console.log(name, destination, firstTrain, frequency, dateAdded);
+
+
+      var firstTrainStart = moment(firstTrain, "hh:mm").subtract(1, "years");
+      console.log("first train: " + firstTrainStart);
+
+      var currentTime = moment();
+      console.log("current time: " + currentTime);
+
+      var timeDiff = moment().diff(moment(firstTrainStart), "minutes");
+      console.log("time diff: " + timeDiff);
+
+      var timeRemainder = timeDiff % frequency;
+      console.log(timeRemainder);
+
+      var minutesAway = frequency - timeRemainder;
+      console.log("Minutes Away: " + minutesAway);
+
+      var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm");
+      console.log("Next arrival: " + nextArrival);
+
+      $("#example").append("<tr class='well'><td class='name'> " + name +
+        " </td><td class='destination'> " + destination +
+        // " </td><td class='firstTrain'> " + firstTrain +
+        " </td><td class='frequency'> " + frequency + 
+        " min </td><td class='nextArrival'>" + nextArrival +
+        "</td><td class='minutesAway'>" + minutesAway + " min</td></tr>");
+
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
