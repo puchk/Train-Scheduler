0cca7e7d5dcce2c4d0727497e3f08d4c2e70356c
puchk <kpuchalski@gmail.com>
2017-03-22 01:19:12 -0700

    Updated readme, minor changes to html and js

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Train-Scheduler
+## HW - {Train-Scheduler}
+
+### Live Link
+ - https://puchk.github.io/Train-Scheduler/
+
+### Instructions/Requirements
+ - When adding trains, administrators should be able to submit the following:
+    - Train Name
+    - Destination
+    - First Train Time -- in military time
+    - Frequency -- in minutes
+    - Code this app to calculate when the next train will arrive; this should be relative to the current time.
+    - Users from many different machines must be able to view same train times.
+
+### Technologies Used
+ - Google Firebase
+ - JQuery
+ - Bootstrap
+ - Moment JS
+
+### Code Example of Pushing Data to Firebase
+    database.ref().push({
+        name: name,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,24 +19,32 @@ var frequency = 0;
 $("#submitButton").on("click", function(event) {
 	event.preventDefault();
 
+	// Value from inputs
 	name = $("#trainName").val().trim();
 	destination = $("#destination").val().trim();
 	firstTrain = $("#firstTrain").val().trim();
 	frequency = $("#frequency").val().trim();
 
-	database.ref().push({
-		name: name,
-		destination: destination,
-		firstTrain: firstTrain,
-		frequency: frequency,
-		dateAdded: firebase.database.ServerValue.TIMESTAMP
-	});
+	// Make sure all fields are complete
+	if ((name === "") || (destination === "") || (firstTrain === "") || (frequency === "")) {
+		alert("Please complete all fields.");
+	}
+	else {
+		database.ref().push({
+			name: name,
+			destination: destination,
+			firstTrain: firstTrain,
+			frequency: frequency,
+			dateAdded: firebase.database.ServerValue.TIMESTAMP
+		});
 
-	$("#trainName").val("");
-	$("#destination").val("");
-	$("#firstTrain").val("");
-	$("#frequency").val("");
-	});
+		// Clear input values
+		$("#trainName").val("");
+		$("#destination").val("");
+		$("#firstTrain").val("");
+		$("#frequency").val("");
+	};
+});
 
 	database.ref().on("child_added", function(childSnapshot) {
       name = childSnapshot.val().name;
@@ -47,27 +55,35 @@ $("#submitButton").on("click", function(event) {
       console.log(name, destination, firstTrain, frequency, dateAdded);
 
 
-      var firstTrainStart = moment(firstTrain, "hh:mm").subtract(1, "years");
+      var firstTrainStart = moment(firstTrain, "HH:mm");
       console.log("first train: " + firstTrainStart);
 
       var currentTime = moment();
       console.log("current time: " + currentTime);
 
-      var timeDiff = moment().diff(moment(firstTrainStart), "minutes");
+      var timeDiff = currentTime.diff(moment(firstTrainStart), "minutes");
       console.log("time diff: " + timeDiff);
 
+    // Makes sure the first train is after the current time
+    if (timeDiff >= 0) {
       var timeRemainder = timeDiff % frequency;
       console.log(timeRemainder);
 
       var minutesAway = frequency - timeRemainder;
       console.log("Minutes Away: " + minutesAway);
 
-      var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm");
+      var nextArrival = moment().add(minutesAway, "minutes").format("HH:mm");
       console.log("Next arrival: " + nextArrival);
+  }
+  	// Uses the first train time if the current time is before this
+  	else {
+  		var minutesAway = timeDiff*-1;
+  		var nextArrival = firstTrainStart.format("HH:mm");
+  	};
 
       $("#example").append("<tr class='well'><td class='name'> " + name +
         " </td><td class='destination'> " + destination +
-        // " </td><td class='firstTrain'> " + firstTrain +
+        " </td><td class='firstTrain'> " + firstTrain +
         " </td><td class='frequency'> " + frequency + 
         " min </td><td class='nextArrival'>" + nextArrival +
         "</td><td class='minutesAway'>" + minutesAway + " min</td></tr>");
